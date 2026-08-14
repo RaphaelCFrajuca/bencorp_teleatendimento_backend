a31cf83f5eb40b62431bd1e95de1190988dd5218
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2026-08-14 04:40:03 -0300

    feat: implementar controlador de usuários com criação de usuários e proteção de rota para administradores

--- src/modules/users/controller/users.controller.ts
+++ src/modules/users/controller/users.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { Roles } from 'src/common/decorators/roles.decorator';
+import { CreateUserDto } from '../dto/create-user.dto';
+import { UserResponseDto } from '../dto/user-response.dto';
+import { Role } from '../enum/role.enum';
+import { UsersService } from '../service/users.service';
 
 @Controller('users')
-export class UsersController {}
+@Roles(Role.ADMIN)
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() dto: CreateUserDto): Promise<UserResponseDto> {
+    return this.usersService.createUser(dto);
+  }
+}
