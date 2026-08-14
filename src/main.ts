e7f710768f840fab1e466f688b6a44c0c7fc2492
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2026-08-14 20:31:41 -0300

    feat: adicionar configuração CORS e endpoints do Swagger

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ['http://localhost:8080'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Trace-Id'],
+    credentials: true,
+  });
+
   const config = new DocumentBuilder()
     .setTitle('Bencorp Teleatendimento API')
     .addBearerAuth()
@@ -13,6 +20,9 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('docs', app, documentFactory);
+  SwaggerModule.setup('swagger', app, documentFactory, {
+    jsonDocumentUrl: 'swagger/json',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
