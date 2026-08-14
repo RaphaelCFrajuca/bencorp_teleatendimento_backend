6520faa260fc6f21658d4802c1315afb7af1bc92
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2026-08-14 01:33:23 -0300

    feat: adicionar configuração de middleware e logging no módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
+import { LoggerModule } from 'nestjs-pino';
+import { loggerModuleOptions } from './common/logging/logger-options';
+import { RequestLoggingInterceptor } from './common/logging/request-logging.interceptor';
+import { TraceIdMiddleware } from './common/logging/trace-id.middleware';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [LoggerModule.forRoot(loggerModuleOptions), UsersModule],
   controllers: [],
-  providers: [],
+  providers: [{ provide: APP_INTERCEPTOR, useClass: RequestLoggingInterceptor }],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TraceIdMiddleware).forRoutes('*');
+  }
+}
