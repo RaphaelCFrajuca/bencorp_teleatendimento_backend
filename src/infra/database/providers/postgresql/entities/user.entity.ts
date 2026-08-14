299c6218b1e9d576a29bea3b56dd52b0b6065808
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2026-08-14 02:02:35 -0300

    feat: adicionar entidade UserEntity com propriedades e enum Role

---
+++ src/infra/database/providers/postgresql/entities/user.entity.ts
@@ -0,0 +1,23 @@
+import { User } from 'src/modules/users/entity/user.entity';
+import { Role } from 'src/modules/users/enum/role.enum';
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+export class UserEntity implements User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ name: 'name', type: 'varchar', length: 255, nullable: false })
+  name: string;
+
+  @Column({ name: 'email', type: 'varchar', length: 255, nullable: false, unique: true })
+  email: string;
+
+  @Column({ name: 'password', type: 'varchar', length: 255, nullable: false })
+  password: string;
+
+  @Column({ name: 'active', type: 'boolean', nullable: false, default: true })
+  active: boolean;
+
+  @Column({ name: 'role', type: 'enum', enum: Role, nullable: false })
+  role: Role;
+}
