916fca3af23fa0dacd30e7374eec666fdd6b698c
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2026-08-14 02:23:15 -0300

    feat: atualizar módulo de autenticação para usar configuração assíncrona do JWT

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,13 +1,20 @@
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
+import { EnvironmentModule } from 'src/common/environment/environment.module';
 import { DatabaseModule } from 'src/infra/database/database.module';
 
 @Module({
   imports: [
     DatabaseModule,
-    JwtModule.register({
-      global: true,
-      signOptions: { expiresIn: '60s' },
+    EnvironmentModule,
+    JwtModule.registerAsync({
+      imports: [EnvironmentModule],
+      inject: ['JWT_SECRET'],
+      useFactory: (jwtSecret: string) => ({
+        global: true,
+        secret: jwtSecret,
+        signOptions: { expiresIn: '60s' },
+      }),
     }),
   ],
   controllers: [],
